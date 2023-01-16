7ed50d69c087847598ae2cd895d8a2ac545369f5
mrrob100 <robarnold100@hotmail.com>
2023-01-16 09:29:01 -0300

    init

---
+++ src/Controller/OrangeElephantController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrangeElephantController extends AbstractController
+{
+    #[Route('/orange/elephant', name: 'app_orange_elephant')]
+    public function index(): Response
+    {
+        return $this->render('orange_elephant/index.html.twig', [
+            'controller_name' => 'OrangeElephantController',
+        ]);
+    }
+}
